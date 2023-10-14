c5bb4872320d4469f81ba54f00816a955cf1220c
Shakhzod Akhtamov <shakakhtamov@gmail.com>
2023-10-14 03:51:30 +0000

    LargestSum

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,19 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        if (nums == null || nums.size() < 2) {
+            return 0;
+        }
+        int largestSum = Integer.MIN_VALUE; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int sum = nums.get(i) + nums.get(j);
+                if (sum > largestSum) {
+                    largestSum = sum;
+                }
+            }
+        }
+        return largestSum;
     }
 }
